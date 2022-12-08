e4c74428bf45cc7295579e49022456eda364a6ea
hskang9 <hyungsuk@aurora.dev>
2022-12-08 19:28:20 +0400

    add view logic for getting policy

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use aurora_workspace::types::output::TransactionStatus;
 use aurora_workspace_demo::common;
 use ethabi::Constructor;
 use ethereum_tx_sign::{LegacyTransaction, Transaction};
-use std::fs::File;
-use serde_json::json;
 use near_units;
+use serde_json::json;
+use std::{fs::File, str::FromStr};
+use workspaces::AccountId;
 
 const PRIVATE_KEY: [u8; 32] = [88u8; 32];
 
@@ -17,7 +18,11 @@ async fn main() -> anyhow::Result<()> {
     let dao_factory = worker.dev_deploy(&wasm).await?;
     println!("Contract Id: {}", dao_factory.id());
     // Init daofactory contract
-    let init_tx = dao_factory.call("new").gas(100000000000000).transact().await?;
+    let init_tx = dao_factory
+        .call("new")
+        .gas(100000000000000)
+        .transact()
+        .await?;
     println!("{:?}", init_tx);
 
     // 2. Define parameters of new dao
@@ -33,17 +38,35 @@ async fn main() -> anyhow::Result<()> {
             "name": "aurora-dao",
             "purpose": "Aurora internal test DAO",
             "metadata": "",
-        }, 
-        "policy": council,
+        },
+        "policy": ["bob.near",  "alice.near"],
     });
     let args_bs64 = base64::encode(&serde_json::to_vec(&args).unwrap());
 
-    let create_new_dao = dao_factory.call("create").args_json(json!({
-        "name": "aurora-dao",
-        "args": args_bs64,
-    })).deposit(10000000000000000000000000).gas(150000000000000).transact().await?;
-    
+    // - Create a new DAO
+    let create_new_dao = dao_factory
+        .call("create")
+        .args_json(json!({
+            "name": "aurora-dao",
+            "args": format!("{}", args_bs64),
+        }))
+        .deposit(10000000000000000000000000)
+        .gas(150000000000000)
+        .transact()
+        .await?;
+
     println!("{:?}", create_new_dao);
+
+    // 3. Get the council deploy contract from dao
+    let aurora_dao_id = AccountId::from_str(&format!("aurora-dao.{}", dao_factory.id()))?;
+
+    println!("Aurora DAO ID: {}", aurora_dao_id);
+    let dao_contract = worker
+        .import_contract(&aurora_dao_id, &worker)
+        .transact()
+        .await?;
+    let get_policy = dao_contract.view("get_policy").await?.json()?;
+    println!("{:?}", get_policy);
 
     Ok(())
 }
