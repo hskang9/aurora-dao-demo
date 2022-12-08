d545cea7ca78f62a127b48c8b3f9fc2915cfb06f
hskang9 <hyungsuk@aurora.dev>
2022-12-08 20:50:15 +0400

    add blob store logic

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use aurora_workspace::types::output::TransactionStatus;
+use aurora_workspace::types::{output::TransactionStatus, SecretKey, KeyType, Account};
 use aurora_workspace_demo::common;
 use ethabi::Constructor;
 use ethereum_tx_sign::{LegacyTransaction, Transaction};
@@ -7,15 +7,14 @@ use serde_json::json;
 use std::{fs::File, str::FromStr};
 use workspaces::AccountId;
 
-const PRIVATE_KEY: [u8; 32] = [88u8; 32];
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     // 1. Create a sandbox environment.
     let worker = workspaces::sandbox().await?;
+
     // Deploy Spunik DAO factory contract in sandbox
     let wasm = std::fs::read("res/sputnikdao_factory2.wasm")?;
-    let dao_factory = worker.dev_deploy(&wasm).await?;
+    let dao_factory = worker.create_tla_and_deploy(AccountId::from_str("dao-factory.test.near")?, SecretKey::from_random(KeyType::ED25519), &wasm).await?.unwrap();
     println!("Contract Id: {}", dao_factory.id());
     // Init daofactory contract
     let init_tx = dao_factory
@@ -28,9 +27,9 @@ async fn main() -> anyhow::Result<()> {
     // 2. Define parameters of new dao
 
     // - Define a council
-    let bob = common::create_account(&worker, "bob.near", None);
-    let alice = common::create_account(&worker, "alice.near", None);
-    let council = ["bob.near", "alice.near"];
+    let bob = common::create_account(&worker, "bob.test.near", None);
+    let alice = common::create_account(&worker, "alice.test.near", None);
+    let council = ["bob.test.near", "alice.test.near"];
 
     // - Configure name, purpose, and initial council members of the DAO and convert the arguments in base64
     let args = json!({
@@ -39,7 +38,7 @@ async fn main() -> anyhow::Result<()> {
             "purpose": "Aurora internal test DAO",
             "metadata": "",
         },
-        "policy": ["bob.near",  "alice.near"],
+        "policy": ["bob.test.near",  "alice.test.near"],
     });
     let args_bs64 = base64::encode(&serde_json::to_vec(&args).unwrap());
 
@@ -65,8 +64,19 @@ async fn main() -> anyhow::Result<()> {
         .import_contract(&aurora_dao_id, &worker)
         .transact()
         .await?;
-    let get_policy = dao_contract.view("get_policy").await?.json()?;
+
+    // - Get policy
+    let get_policy = dao_contract.view("get_policy").await?;
     println!("{:?}", get_policy);
+    let a = worker.root_account()?;
+    let mint_near = a.transfer_near(&aurora_dao_id, 10000000000000000000000000).await?;
+    println!("{:?}", mint_near);
+
+    // - Get council to add store blob for aurora deployment code (aurora-testnet.wasm)
+    // get worker account more balance
+    let aurora_wasm = std::fs::read("res/aurora-testnet.wasm")?;
+    let store_blob = dao_contract.call("store_blob").args(aurora_wasm).deposit(9534940000000000000000000).gas(100054768750000).transact().await?;
+    println!("{:?}", store_blob);
 
     Ok(())
 }
